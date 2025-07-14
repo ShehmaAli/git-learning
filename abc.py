1d43a68701d0897b7ce1ff7ac76033503a135c96
Shehma Ali <shehmaali08@gmail.com>
2025-07-14 18:13:00 +0500

    major changes to code

--- abc.py
+++ abc.py
@@ -1,9 +1,8 @@
-a = 10
-b = 20
-c = a + b
-
-d = 20 // 3
-
+quiz = {
+    "what is the colour of the sky? ": "blue",
+    "how many planets are there in our solar system? ": "8",
+    "Who played the iron man in the avengers endgame? ": "robert downey junior"
+}
 """
 git remote
 git commit ✔️
@@ -13,6 +12,16 @@ git pull ✔️
 git push ✔️test change
 
 """
+answers = []
+name = input("What is your name? ")
+decision = input("Do you want answer some questions? (y for yes & n for no) ")
 
-print(a, b, c, d)
-# brach main
+if decision == "y":
+    print(f"hello {name}")
+    for q in quiz.keys():
+        answer = input(q)
+        answers.append(answer)
+if answers == list(quiz.values()):
+    print("yes")
+else:
+    print("no")
